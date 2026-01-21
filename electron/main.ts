47ddcebb82106e2f055868672fbf7099361e02cc
Zygarde641 <Zygarde641@Gmail.com>
2026-01-21 19:45:00 +0530

    fix: correct production build path for index.html

--- electron/main.ts
+++ electron/main.ts
@@ -127,9 +127,8 @@ function createWindow() {
         mainWindow.loadURL('http://localhost:5173');
         mainWindow.webContents.openDevTools();
     } else {
-        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-        // TEMPORARY: Open DevTools in production to debug loading issue
-        mainWindow.webContents.openDevTools();
+        // Production: load from the correct path
+        mainWindow.loadFile(path.join(__dirname, '..', 'index.html'));
     }
 
     mainWindow.on('moved', () => {
